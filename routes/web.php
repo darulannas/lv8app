e9ad49a4f4997c106e8a3b3b69b8bcd176ce7285
darulannas <darulannas7@gmail.com>
2023-01-21 00:41:33 +0700

    structure, routes, views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Darul Annas",
+        "email" => "darulannas7@gmail.com",
+        "image" => "profil-image.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
