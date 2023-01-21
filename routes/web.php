8a5dc66292d3de20d432979ad523faf830c375b7
darulannas <darulannas7@gmail.com>
2023-01-21 15:00:57 +0700

    blade templating engine

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,69 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Darul Annas",
         "email" => "darulannas7@gmail.com",
         "image" => "profil-image.jpeg"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Darul",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad qui facilis quis laboriosam omnis, molestias, dolores ab distinctio modi consectetur eius facere asperiores beatae nam atque. Doloremque necessitatibus incidunt quo doloribus ut eveniet quae ducimus autem ab corporis deleniti praesentium, nemo, explicabo laboriosam voluptates neque excepturi similique veritatis? Nam iste quae saepe sapiente perspiciatis incidunt iusto cupiditate cum debitis. Perspiciatis ab ratione excepturi voluptatem suscipit ullam neque deleniti veniam, vero eligendi et doloribus a odio dolor quo in repellendus eveniet!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Annas",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad qui facilis quis laboriosam omnis, molestias, dolores ab distinctio modi consectetur eius facere asperiores beatae nam atque. Doloremque necessitatibus incidunt quo doloribus ut eveniet quae ducimus autem ab corporis deleniti praesentium, nemo, explicabo laboriosam voluptates neque excepturi similique veritatis? Nam iste quae saepe sapiente perspiciatis incidunt iusto cupiditate cum debitis. Perspiciatis ab ratione excepturi voluptatem suscipit ullam neque deleniti veniam, vero eligendi et doloribus a odio dolor quo in repellendus eveniet!"
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Darul",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad qui facilis quis laboriosam omnis, molestias, dolores ab distinctio modi consectetur eius facere asperiores beatae nam atque. Doloremque necessitatibus incidunt quo doloribus ut eveniet quae ducimus autem ab corporis deleniti praesentium, nemo, explicabo laboriosam voluptates neque excepturi similique veritatis? Nam iste quae saepe sapiente perspiciatis incidunt iusto cupiditate cum debitis. Perspiciatis ab ratione excepturi voluptatem suscipit ullam neque deleniti veniam, vero eligendi et doloribus a odio dolor quo in repellendus eveniet!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Annas",
+            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad qui facilis quis laboriosam omnis, molestias, dolores ab distinctio modi consectetur eius facere asperiores beatae nam atque. Doloremque necessitatibus incidunt quo doloribus ut eveniet quae ducimus autem ab corporis deleniti praesentium, nemo, explicabo laboriosam voluptates neque excepturi similique veritatis? Nam iste quae saepe sapiente perspiciatis incidunt iusto cupiditate cum debitis. Perspiciatis ab ratione excepturi voluptatem suscipit ullam neque deleniti veniam, vero eligendi et doloribus a odio dolor quo in repellendus eveniet!"
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
