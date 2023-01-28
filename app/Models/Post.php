bf4a4017c05cfb05ac882fb527c4c53ad2f06cc0
darulannas <darulannas7@gmail.com>
2023-01-28 22:56:51 +0700

    post model 1

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,38 +2,10 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Darul",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad qui facilis quis laboriosam omnis, molestias, dolores ab distinctio modi consectetur eius facere asperiores beatae nam atque. Doloremque necessitatibus incidunt quo doloribus ut eveniet quae ducimus autem ab corporis deleniti praesentium, nemo, explicabo laboriosam voluptates neque excepturi similique veritatis? Nam iste quae saepe sapiente perspiciatis incidunt iusto cupiditate cum debitis. Perspiciatis ab ratione excepturi voluptatem suscipit ullam neque deleniti veniam, vero eligendi et doloribus a odio dolor quo in repellendus eveniet!"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Annas",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad qui facilis quis laboriosam omnis, molestias, dolores ab distinctio modi consectetur eius facere asperiores beatae nam atque. Doloremque necessitatibus incidunt quo doloribus ut eveniet quae ducimus autem ab corporis deleniti praesentium, nemo, explicabo laboriosam voluptates neque excepturi similique veritatis? Nam iste quae saepe sapiente perspiciatis incidunt iusto cupiditate cum debitis. Perspiciatis ab ratione excepturi voluptatem suscipit ullam neque deleniti veniam, vero eligendi et doloribus a odio dolor quo in repellendus eveniet!"
-        ]
-    ];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        // $post = [];
-        // foreach ($posts as $p) {
-        //     if ($p["slug"] === $slug) {
-        //         $post = $p;
-        //     }
-        // }
-
-        return $posts->firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,8 @@
 <h1 class="mb-3">Halaman Blog Posts</h1>
     @foreach($posts as $posts)
     <article class="mb-5">
-        <h2><a href="/posts/{{ $posts["slug"] }}">{{ $posts["title"] }}</a></h2>
-        <h5>By: {{ $posts["author"] }}</h5>
-        <p>{{ $posts["body"] }}</p>
+        <h2><a href="/posts/{{ $posts->id }}">{{ $posts->title }}</a></h2>
+        <p>{{ $posts->body }}</p>
     </article>
     @endforeach
 
