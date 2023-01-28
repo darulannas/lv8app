28108dd14bdcf43da6532e791f41386f739ca90d
darulannas <darulannas7@gmail.com>
2023-01-28 23:44:20 +0700

    post model 3

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts/main')
 
 @section('container')
-<h1 class="mb-3">Halaman Blog Posts</h1>
+<h1 class="mb-4">Halaman Blog Posts</h1>
     @foreach($posts as $posts)
-    <article class="mb-5">
-        <h2><a href="/posts/{{ $posts->id }}">{{ $posts->title }}</a></h2>
+    <article class="mb-3">
+        <h2><a href="/posts/{{ $posts->slug }}">{{ $posts->title }}</a></h2>
         <p>{{ $posts->excerpt }}</p>
     </article>
     @endforeach
