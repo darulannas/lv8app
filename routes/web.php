8eb7480da941ddd10c90debf85a92a09c31d255a
darulannas <darulannas7@gmail.com>
2023-02-10 06:52:50 +0700

    login and registration 2

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,6 +49,8 @@ Route::get('/categories', function () {
 });
 
 Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 // Route::get('/categories/{category:slug}', function (Category $category) {
 //     return view('posts', [
