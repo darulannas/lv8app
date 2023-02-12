fec3c327c3ea868ba28a89061abc1e9d68472ea8
darulannas <darulannas7@gmail.com>
2023-02-12 07:44:43 +0700

    user login and middleware

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,26 +12,32 @@
       @endif
 
         <main class="form-signin w-100 m-auto">
-            <form>
+            <form action="/login" method="post">
+            @csrf
             <div class="d-flex justify-content-center mt-5">
               <img class="mb-4" src="img/logo-rji.png" alt="logo rji" width="150" height="150">
             </div>
 
             <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-          
+    
               <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                <label for="email">Email address</label>
+                @error('email')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+                @enderror
               </div>
               <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                <label for="password">Password</label>
               </div>
           
               <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
             <small class="d-block text-center mt-3">Not Registed? <a href="/register">Register Now!</a></small>
-          </main>
+        </main>
     </div>
 </div>
 
